c09d25c8d5079d412454591d7bae1f3596365a69
limsol02 <yesshol@naver.com>
2024-02-01 09:19:07 +0900

    taskDetail dao 수정

--- tryForge-1/src/main/java/com/web/spring/task/service/TaskService.java
+++ tryForge-1/src/main/java/com/web/spring/task/service/TaskService.java
@@ -2,6 +2,7 @@ package com.web.spring.task.service;
 
 import java.util.List;
 
+import com.web.spring.gantt.dao.GanttDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,8 @@ import com.web.spring.vo.Task;
 public class TaskService {
 	@Autowired(required = false)
 	private TaskDao dao;
+	@Autowired(required = false)
+	private GanttDao ganttdao;
 	
 	// 업무 출력(jsp에서 요청상태 확인)
 	public List<Task> getTask(){
@@ -20,5 +23,9 @@ public class TaskService {
 	// 업무 확인
 	public String uptConfirm(int id){
 		return dao.uptConfirm(id)>0?"업무 확인 완료":"업무 확인 에러";
+	}
+	// 해당 업무 디테일
+	public Task taskDetail(int id){
+		return ganttdao.taskDetail(id);
 	}
 }
