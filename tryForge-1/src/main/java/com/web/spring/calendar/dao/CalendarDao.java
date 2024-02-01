1b7589df5c0a188bd2a6c4744db66bdb1d203821
hyeon2z <148454038+hyeon2z@users.noreply.github.com>
2024-02-01 11:48:25 +0900

    캘린더 프로젝트키/유저이름 연동

--- tryForge-1/src/main/java/com/web/spring/calendar/dao/CalendarDao.java
+++ tryForge-1/src/main/java/com/web/spring/calendar/dao/CalendarDao.java
@@ -2,13 +2,14 @@ package com.web.spring.calendar.dao;
 
 import java.util.List;
 
+import com.web.spring.vo.Project;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.web.spring.vo.Calendar;
 
 @Mapper
 public interface CalendarDao {
-	List<Calendar> getCalendar(); // 조회
+	List<Calendar> getCalendar(Project project); // 조회
 	
 	int insertCalendar(Calendar ins); // 추가
 	
