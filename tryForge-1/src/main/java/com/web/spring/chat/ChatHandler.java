28270aa1447da7e073caf85317b9834010907157
seO-Oyen <kky77209@gmail.com>
2024-02-06 12:27:27 +0900

    httpsession으로 구분할수 있도록 처리(살짝 미완성)

--- tryForge-1/src/main/java/com/web/spring/chat/ChatHandler.java
+++ tryForge-1/src/main/java/com/web/spring/chat/ChatHandler.java
@@ -8,11 +8,13 @@ import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.web.spring.vo.Member;
 // chatHandler
 @Component
 public class ChatHandler extends TextWebSocketHandler{
 	// 접속한 채팅 소켙세션(접속자 저장)
-//	private Map<String, WebSocketSession > users = 
+//	private Map<String, WebSocketSession > CLIENTS = 
 //			new ConcurrentHashMap<>();
 	private static final ConcurrentHashMap<String, WebSocketSession> CLIENTS = new ConcurrentHashMap<String, WebSocketSession>();
 	
@@ -21,11 +23,34 @@ public class ChatHandler extends TextWebSocketHandler{
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		// TODO Auto-generated method stub
 		super.afterConnectionEstablished(session);
+		
 		// session.getId() : 소켓서버에서 발급된 고유 id(0부터시작해서 16진수값으로 설정)
 		System.out.println(session.getId()+"님 소켓 서버에 접속했습니다.");
-		// 접속시 접속자 정보에 아이디와 소켓세션 저장..
-		CLIENTS.put(session.getId(),session);
+		Map map = (Map)session.getAttributes();
+		Member member = (Member)map.get("loginMem");
+//		if (member != null) {
+//			System.out.println(member.getMember_id()+"님 소켓 서버에 접속했습니다.");
+//			CLIENTS.put(member.getMember_id(), session);
+//		} else {
+//			CLIENTS.put(session.getId(),session);
+//		}
+		
+		// 접속시 접속자 정보에 아이디와 소켓세션 저장
+		System.out.println(member.getMember_id()+"님 소켓 서버에 접속했습니다.");
+		
+		// 테스트
+		System.out.println("현재 접속자");
+		for(WebSocketSession ws: CLIENTS.values()) {
+			Map map2 = (Map)ws.getAttributes();
+			Member mem = (Member)map2.get("loginMem");
+			
+			System.out.println(mem.getMember_name());
+		}
+		
+		CLIENTS.put(member.getMember_id(), session);
+		
 	}
+	
 	// 메시지보낼 때
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
@@ -36,8 +61,15 @@ public class ChatHandler extends TextWebSocketHandler{
 			message.getPayload());
 		// 소켓 서버에 접속한 사람들
 		for(WebSocketSession ws: CLIENTS.values()) {
-			System.out.println(ws.getId()+"에게 메시지 발송");
-			ws.sendMessage(message);
+			Map map = (Map)ws.getAttributes();
+			Member member = (Member)map.get("loginMem");
+			if (member != null) {
+				System.out.println(member.getMember_id()+"에게 메시지 발송");
+				ws.sendMessage(message);
+			} else {
+				System.out.println(ws.getId()+"에게 메시지 발송");
+				ws.sendMessage(message);
+			}
 			
 		}
 	}
@@ -46,9 +78,12 @@ public class ChatHandler extends TextWebSocketHandler{
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		// TODO Auto-generated method stub
 		super.afterConnectionClosed(session, status);
-		System.out.println(session.getId()+"님 접속 종료!!");
-		CLIENTS.remove(session.getId());
+		Map map = (Map)session.getAttributes();
+		Member member = (Member)map.get("loginMem");
+		System.out.println(member.getMember_name() + "님 접속 종료!!");
+		CLIENTS.remove(member.getMember_id());
 	}
+	
 	// 에러발생시
 	@Override
 	public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
--- tryForge-1/src/main/java/com/web/spring/chat/WebSocketConfig.java
+++ tryForge-1/src/main/java/com/web/spring/chat/WebSocketConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,7 +17,9 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(chatHandler, "/ws/chat").setAllowedOrigins("*");
+		registry.addHandler(chatHandler, "/ws/chat")
+		.addInterceptors(new HttpSessionHandshakeInterceptor())
+		.setAllowedOrigins("*");
 	}
 
 }
