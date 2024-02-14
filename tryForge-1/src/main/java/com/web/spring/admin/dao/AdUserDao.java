de9f6ef3e2727ea5d80e19f4a576efdf54002365
limsol02 <yesshol@naver.com>
2024-02-14 15:48:27 +0900

    ??

--- tryForge-1/src/main/java/com/web/spring/admin/dao/AdUserDao.java
+++ tryForge-1/src/main/java/com/web/spring/admin/dao/AdUserDao.java
@@ -1,13 +1,15 @@
 package com.web.spring.admin.dao;
- // com.web.spring.admin.dao.AdUserDao
+ import java.util.List;
+
+// com.web.spring.admin.dao.AdUserDao
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.web.spring.vo.Team_Member;
-import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface AdUserDao {
-	Team_Member tmFromMember(int member_key);
+	List<Team_Member> tmFromMember(int member_key);
 	// PL 변경
 	int assignPL(@Param("team_Member_key")int team_Member_key);
 	// 새 구성원 추가
